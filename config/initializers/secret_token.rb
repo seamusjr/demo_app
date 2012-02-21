87061c6e6dd6e9987d7dac4cd5c09b8c7d5cfea1
Seamus Roche <seamusjr@gmail.com>
2012-02-21 17:56:24 -0600

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorTutApp::Application.config.secret_token = 'f3982d625a53abfaea6e0a3f8713b63f18d2e59f007b10552eeddae5cf4dc0b936483eb94d4fd8b8f6778dbeb47c09d9fb31b13d8bdeb72d8a15d08f3a6d18c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RorTutApp::Application.config.session_store :cookie_store, key: '_ror_tut_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RorTutApp::Application.config.session_store :active_record_store
